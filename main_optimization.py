0b6a6bfbe82058e417e9f0823b20f99197fe5b4e
RoopeshPyneni <roopesh.pyneni@gmail.com>
2021-09-03 10:32:28 +0530

    Minor Changes

--- main_optimization.py
+++ main_optimization.py
@@ -361,7 +361,7 @@ file_choose='S' # 'S' to run a single time; 'M' to run multiple times
 if file_choose=='S':
 
 	# ------- Set Any Additional Parameters Here --------
-	filename=f_directory+'test_main_change_2'						# SET THE FILENAME HERE
+	filename=f_directory+'test_file_input_change_1'						# SET THE FILENAME HERE
 	optimization_input_parameters['optimization']['max_iteration']=300	
 	# ------- Set Any Additional Parameters Here --------
 	
