e41694bee02d102460b4816d39bcb378ddcce9e6
RoopeshPyneni <roopesh.pyneni@gmail.com>
2021-11-12 08:31:52 +0530

    New hand calculations with Qin

--- CS_LNA/pre_optimization.py
+++ CS_LNA/pre_optimization.py
@@ -21,6 +21,7 @@ import datetime
 import CS_LNA.extra_function as cff		# type: ignore
 import CS_LNA.hand_calculation_1 as hc1 # type: ignore
 import CS_LNA.hand_calculation_2 as hc2 # type: ignore
+import CS_LNA.hand_calculation_3 as hc3 # type: ignore
 
 
 #===========================================================================================================================
@@ -174,10 +175,11 @@ def pre_optimization(cir,optimization_input_parameters,timing_results):
 	
 	#======================================================== Automatic Initial Points =============================================================================================================
 
+	
 	if optimization_input_parameters['pre_optimization']['type']==1:
 		
 		print('************************************************************************************************************')
-		print('*********************************** MOSFET Parameters ******************************************************')
+		print('***********************************  Automatic Operating Point Selection 1 *********************************')
 
 		# Extracting the MOSFET Parameters from the MOS file
 		hc1.automatic_initial_parameters(cir,optimization_input_parameters,optimization_results)
@@ -185,10 +187,18 @@ def pre_optimization(cir,optimization_input_parameters,timing_results):
 	if optimization_input_parameters['pre_optimization']['type']==2:
 
 		print('************************************************************************************************************')
-		print('*********************************** MOSFET Parameters ******************************************************')
+		print('***********************************  Automatic Operating Point Selection 2 *********************************')
 
 		# Extracting the MOSFET Parameters from the MOS file
 		hc2.automatic_initial_parameters(cir,optimization_input_parameters,optimization_results)
+	
+	if optimization_input_parameters['pre_optimization']['type']==3:
+
+		print('************************************************************************************************************')
+		print('***********************************  Automatic Operating Point Selection 3 *********************************')
+
+		# Extracting the MOSFET Parameters from the MOS file
+		hc3.automatic_initial_parameters(cir,optimization_input_parameters,optimization_results)
 
 	# Printing the values
 	cff.print_circuit_parameters(cir.circuit_parameters)
