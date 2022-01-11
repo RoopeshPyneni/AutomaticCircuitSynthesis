525902f4bb37acca99d02885da563e2469b612c8
RoopeshPyneni <roopesh.pyneni@gmail.com>
2022-01-11 19:18:41 +0530

    Minor Changes

--- main_optimization_CS_LNA.py
+++ main_optimization_CS_LNA.py
@@ -190,7 +190,7 @@ def get_optimization_parameters(optimization_input_parameters,fo,optimization_na
 	#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 	# Optimization Run 1
 	optimization_input_parameters['optimization'][1]={}
-	optimization_input_parameters['optimization'][1]['max_iteration']=600
+	optimization_input_parameters['optimization'][1]['max_iteration']=6
 	optimization_input_parameters['optimization'][1]['alpha_min']=-1
 	optimization_input_parameters['optimization'][1]['consec_iter']=-1
 	optimization_input_parameters['optimization'][1]['delta_threshold']=0.001
@@ -506,7 +506,7 @@ file_choose='S' # 'S' to run a single time; 'M' to run multiple times
 if file_choose=='S':
 
 	# ------- Set Any Additional Parameters Here --------
-	filename=f_directory+'Optimization_50_MHz'						# SET THE FILENAME HERE
+	filename=f_directory+'Test_Optimization'						# SET THE FILENAME HERE
 	# ------- Set Any Additional Parameters Here --------
 	
 
