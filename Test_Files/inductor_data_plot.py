c18a549e03b7325ba9e2e23178f4a09fed764e84
RoopeshPyneni <roopesh.pyneni@gmail.com>
2022-02-14 13:05:54 +0530

    Corrections to Q,L plot code

--- Test_Files/inductor_data_plot.py
+++ Test_Files/inductor_data_plot.py
@@ -13,30 +13,73 @@ import pandas as pd
 from matplotlib import pylab
 from pylab import *
 
+
+"""
+============================================================================================================================
+---------------------------------------------- DATA EXTRACTION -------------------------------------------------------------
+"""
+
+def extract_data(file_name):
+
+	# Reading the data from the csv file
+	f=open(file_name,'r')
+	lines=f.readlines()
+	f.close()
+
+	# Initializing a dictionary
+	output_dict={}
+	header_dict={}
+
+	# Reading line 0 ( HEADER )
+	line=lines[0]
+	lines=lines[1:]
+	line=line.split('\n')[0]
+	line=line.split(',')
+	i=0
+	for name in line:
+		output_dict[name]=[]
+		header_dict[i]=name
+		i+=1
+
+	# Reading other lines
+	n_rows=0
+	while lines!=[]:
+		n_rows+=1
+		line=lines[0]
+		lines=lines[1:]
+		line=line.split(',')
+		i=0
+		for val in line:
+			output_dict[header_dict[i]].append(float(val))
+			i+=1
+	
+	return output_dict,n_rows
+
+
 """
 ============================================================================================================================
 ---------------------------------------------- PLOTS -----------------------------------------------------------------------
 """
 
-def plot_single_data(data,circuit_parameters,param_name):
+def plot_single_data(data,n_rows,circuit_parameters,param_name):
 	
 	Q_array=[]
 	L_array=[]
 	param_array=[]
 
-	for i in range(data.shape[0]):
+	for i in range(n_rows):
 		flag=0
 		for param in circuit_parameters:
 			if param==param_name:
 				continue
-			if data.at[i,param]!=circuit_parameters[param]:
+			if data[param][i]!=circuit_parameters[param]:
 				flag=1
 				break
 		if flag==1:
 			continue
-		Q_array.append(data.at[i,'Q'])
-		L_array.append(data.at[i,'L'])
-		param_array.append(data.at[param_name,'Q'])
+		Q_array.append(data['Q'][i])
+		L_array.append(data['L'][i])
+		param_array.append(data[param_name][i])
 
 	figure()
 	plot(param_array,L_array)
@@ -66,8 +109,7 @@ file_directory_output='/home/ee18b028/Optimization/Simulation_Results/Inductor/S
 file_name=file_directory_output+'inductor_sweep.csv'
 
 # Reading the csv data
-data=pd.read_csv(file_name)
-n_rows=data.shape[0]
+data,n_rows=extract_data(file_name)
 
 circuit_parameters={
 	'Width':10e-6,
