2f13d32ac46db55948beba3281b105f0156e0c18
RoopeshPyneni <roopesh.pyneni@gmail.com>
2022-02-21 21:51:07 +0530

    Minor Changes

--- Test_Files/inductor_data_sort.py
+++ Test_Files/inductor_data_sort.py
@@ -13,6 +13,28 @@ from matplotlib import pylab
 from pylab import *
 
 
+
+"""
+============================================================================================================================
+------------------------------------------ OTHER FUNCTION ------------------------------------------------------------------
+"""
+
+#--------------------------------------------------------------------------------
+# This function will sort two lists
+def sort_2(list1,list2,**kwargs):
+    list_zip=list(zip(list1,list2))
+    if 'order' in kwargs:
+        if kwargs['order']=='D':
+            list_zip=sorted(list_zip,reverse=True)
+        else:
+            list_zip=sorted(list_zip,reverse=False)
+    else:
+        list_zip=sorted(list_zip,reverse=False)
+    list1=[i for (i,s) in list_zip]
+    list2=[s for (i,s) in list_zip]
+    return list1,list2
+    
+    
 """
 ============================================================================================================================
 ------------------------------------------ EXTRACTION FUNCTION -------------------------------------------------------------
@@ -36,11 +58,12 @@ def extract_data(file_name):
 	line=line.split('\n')[0]
 	line=line.split(',')
 	for name in line:
-		column_dict[n_col]=column_dict
+		column_dict[n_col]=name
 		data_dict[name]=[]
 		n_col+=1
 
 	# Getting the data
+	n_rows=0
 	while lines!=[]:
 		line=lines[0]
 		lines=lines[1:]
@@ -50,28 +73,24 @@ def extract_data(file_name):
 		for name in line:
 			data_dict[column_dict[i]].append(float(name))
 			i+=1
+		n_rows+=1
+		
+		if n_rows%10000==0:
+			print(n_rows)
+	
+	#print(data_dict)
+	print(n_rows)
 
 	return data_dict
 
 #---------------------------------------------------------------------------------------------------------------------------
 # Sorting the data
 def sort_data(data_dict):
-	L_array=data_dict['L']
-	n_rows=len(L_array)
-
-	new_L=[]
-	for i in range(n_rows):
-		i_min=-1
-		L_min=1e15
-		for j in range(n_rows):
-			if j in new_L:
-				continue
-			if L_array[j]<L_min:
-				i_min=j
-				L_min=L_array[j]
-		
-		if i_min>-1:
-			new_L.append(i_min)
+	
+	L_array=data_dict['L'].copy()
+	new_L=np.arange(len(L_array))
+	
+	L_array,new_L=sort_2(L_array,new_L)
 	
 	return new_L
 
@@ -112,9 +131,18 @@ def store_data(filename,data_dict,new_L):
 """
 
 # Filenames for the netlist file
-file_directory='/home/ee18b028/cadence_project/Test_Circuits/Inductor/Inductor_Test_1/'
+file_directory='/home/ee18b028/Optimization/Simulation_Results/Inductor/Sweep2/'
+
+print('Starting Data Extract')
+print(datetime.datetime.now())
 data_dict=extract_data(file_directory+'inductor_sweep.csv')
+
+print('Starting Data Sort')
+print(datetime.datetime.now())
 new_L=sort_data(data_dict)
+
+print('Starting Data Write')
+print(datetime.datetime.now())
 store_data(file_directory+'inductor_sweep_1.csv',data_dict,new_L)
 
 
