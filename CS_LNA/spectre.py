c95e010580eac0af2452df0f6eb96c6ac6142814
RoopeshPyneni <roopesh.pyneni@gmail.com>
2022-04-03 17:59:17 +0530

    Minor Changes

--- CS_LNA/spectre.py
+++ CS_LNA/spectre.py
@@ -993,7 +993,7 @@ def write_extract(circuit_parameters,circuit_initialization_parameters):
 		# Creating spectre run directory
 		spectre_folder=circuit_initialization_parameters_run[i]['simulation']['standard_parameters']['tcsh']+'Spectre_Run/'
 		spectre_path=spectre_folder+'T'+str(i)+'/'
-		if not os.path.exists(spectre_folder):
+		if not os.path.exists(spectre_path):
 			shutil.copytree(spectre_folder+'T_extra/',spectre_path)
 
 		circuit_initialization_parameters_run[i]['simulation']['standard_parameters']['directory']=netlist_path
--- Spectre_Run/T4/spectre_run.tcsh
+++ Spectre_Run/T4/spectre_run.tcsh
@@ -1,5 +1,5 @@
 #tcsh
 source ~/.cshrc
-cd /home/ee18b028/cadence_project/CS_LNA/T3/iip3_hb_rcl
+cd /home/ee18b028/cadence_project/CS_LNA/T4/iip3_hb_rcl
 spectre circ.scs =log circ_log.txt
 exit
