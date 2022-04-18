ddb9fb71b14d34257e3ce8557f4e52eb56f244c0
RoopeshPyneni <roopesh.pyneni@gmail.com>
2022-04-18 21:07:37 +0530

    Small Change

--- CS_LNA/spectre.py
+++ CS_LNA/spectre.py
@@ -70,9 +70,12 @@ class Circuit():
 	
 	# Running multiple circuits 
 	def run_circuit_multiple(self,initial_circuit_parameters_dict):
+		print('\n\n\n******************* MULTIPLE RUNS ********************************\n\n\n')
+		print(initial_circuit_parameters_dict)
 		circuit_parameters_dict={}
 		for i in initial_circuit_parameters_dict:
-			circuit_parameters_dict[i]=get_final_circuit_parameters(initial_circuit_parameters_dict[i],self.circuit_initialization_parameters)
+			circuit_parameters_dict[i]=get_final_circuit_parameters(initial_circuit_parameters_dict[i],self.circuit_initialization_parameters).copy()
+		print(circuit_parameters_dict)
 		extracted_parameters_dict=write_extract(circuit_parameters_dict,self.circuit_initialization_parameters)
 		return extracted_parameters_dict
 
